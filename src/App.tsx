cb638322d655eb51ea0ee4142e235a8bffeb0c40
charliemah <teamnova@myyahoo.com>
2024-05-05 14:11:42 -0700

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
 import osssomeLogo from '/osssome-2.png'
+import osomeWeekendLogo from '/osomeweekend.png'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <img src={osssomeLogo} className="logo" alt="Osssome logo" />
-      <div className="title">
+      <div className="hero">
         <h1 className="highlight">Coming Soon</h1>
-        <h2>The Topcoder for</h2>
-        <h2>        
-          <span className="highlight">Open Source&nbsp;</span>
-          Software Challenges
+        <br/>
+        <br/>
+        <a href="https://osomeweekend.super.site" target="_blank">
+          <img src={osomeWeekendLogo} className="logo1" alt="OsomeWeekend logo" />
+        </a>
+        <h2>
+          The first&nbsp;
+          <span className="highlight">all-in-one&nbsp;</span>
+          platform for&nbsp;
+          <span className="highlight">Startup Weekend&nbsp;</span>
+          events
         </h2>
+        <br/>
+        <br/>
+        <div className="demo">
+          <a href="https://osomeweekend.super.site/" target="_blank">
+            <button id="counter" type="button">
+              See the Demo
+            </button>
+          </a>
+        </div>
+        <br/>
       </div>
-      <div className="tagline">
-        <h3 className="highlight">We help awesome talents and companies</h3>
-        <h3>focus on the most important things first.</h3>
+      <div className="company">
+        <img src={osssomeLogo} className="logo2" alt="Osssome logo" />
+        <h3>We help awesome people</h3>
+        <h3>address very important challenges!</h3>
       </div>
     </>
   )
